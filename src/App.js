cad40d84fc9445c6a0dbfaa03144210fa4e33d17
bgeorgieff <b.g.georgiev87@gmail.com>
2021-09-18 21:42:53 +0300

    feat/projects-page

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ import { Switch, Route } from 'react-router-dom'
 import { About } from './Pages/About'
 import { Projects } from './Pages/Projects'
 import { ContactPage } from './Pages/Contact'
+import { useState } from 'react'
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false)
+
+  const navClick = () => {
+    setNavToggle(!navToggle)
+  }
+
   return (
     <div className="App">
       <Particles
@@ -28,10 +35,16 @@ function App() {
           }
         }}}
       />
-      <div className='sidebar'>
+      <div className={`sidebar${navToggle ? ' nav-toggle' : ''}`}>
         <NavBar />
       </div>
-      <div className='main-content'>      
+
+      <div className='main-content'>  
+        <div className='nav-btn' onClick={navClick}>
+          <div className='lines-1'></div>
+          <div className='lines-2'></div>
+          <div className='lines-3'></div>
+        </div>    
         <div className='content'>
           <Switch>
             <Route path='/' exact>
--- src/Components/ProjectList.js
+++ src/Components/ProjectList.js
@@ -1,7 +1,30 @@
-export const ProjectList = () => {
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+export const ProjectList = ({list}) => {
+
   return (
-    <div>
-      
+    <div className='project-l-container'>
+      {
+        list.map((item) => 
+        <div className='project' key={item.id}>
+          <div className='project-item'>
+            <div className='project-item-p'>
+              <img src={item.picture} alt='' />
+              <ul className='project-icon'>
+                <li>
+                  <a href={item.link}><FontAwesomeIcon icon={faGithub} /></a>
+                </li>
+              </ul>
+            </div>
+            <a href={item.link}> 
+              <h5>{item.name}</h5>
+              <p>{item.description}</p>
+            </a>
+          </div>
+        </div>
+        )
+      }
     </div>
   )
 }
